215ca66c5c3ae14f539c5b3af50c78d34dde35dc
abbyli437 <31529490+abbyli437@users.noreply.github.com>
2021-06-24 16:21:06 -0700

    implemented collection detail view
    implemented collection detail view so users can access the backdrop, poster, title, and synopsis of the alien movies too

---
+++ Flix/View Controllers/CollectionDetailsViewController.h
@@ -0,0 +1,18 @@
+//
+//  CollectionDetailsViewController.h
+//  Flix
+//
+//  Created by Abby Li on 6/24/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CollectionDetailsViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *movie;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Flix/View Controllers/MoviesGridViewController.m
+++ Flix/View Controllers/MoviesGridViewController.m
@@ -8,6 +8,7 @@
 #import "MoviesGridViewController.h"
 #import "MovieCollectionCell.h"
 #import "UIImageView+AFNetworking.h"
+#import "CollectionDetailsViewController.h"
 
 @interface MoviesGridViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
 
@@ -56,15 +57,21 @@
     [task resume];
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    UITableViewCell *tappedCell = sender;
+    NSIndexPath *indexPath = [self.collectionView indexPathForCell:tappedCell];
+    NSDictionary *movie = self.movies[indexPath.item];
+    
+    CollectionDetailsViewController *cDetailsViewController = [segue destinationViewController];
+    cDetailsViewController.movie = movie;
 }
-*/
+
 
 - (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
     MovieCollectionCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"MovieCollectionCell" forIndexPath:indexPath];
